14e9878d592cee65f2768e14c67280b60ffe8b3d
IT24100637 <it24100637@my.sliit.lk>
2025-10-04 09:10:37 +0530

    Add IT24100637_PS_Lab09/2025 to Lab Sheet 09 Upload files

---
+++ Lab Sheet 09/IT24100637_PS_Lab09/IT24100637_PS_Lab09.R
@@ -0,0 +1,7 @@
+setwd("C:\\Users\\ASUS\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\PS\\IT24100637_PS_Lab09")
+
+#1
+times <- c(44, 47, 45, 46, 43, 45, 44, 48, 46, 47, 45, 46, 44, 45, 47, 46, 45, 44, 46, 47, 45, 46, 44, 45, 46)
+
+#2
+t.test(times, mu = 46, alternative = "less")
